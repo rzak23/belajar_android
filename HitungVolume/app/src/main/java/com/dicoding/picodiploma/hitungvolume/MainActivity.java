683ebb55870e54fa934da3a4898c0a274857074e
Reza Akbar Hidayat <50098895+rzak23@users.noreply.github.com>
2020-01-13 23:24:12 +0800

    upload code

---
+++ HitungVolume/app/src/main/java/com/dicoding/picodiploma/hitungvolume/MainActivity.java
@@ -0,0 +1,103 @@
+package com.dicoding.picodiploma.hitungvolume;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText edtWidth, edtLength, edtHeight;
+    private Button btnCalculate;
+    private TextView tvResult;
+    private static final String STATE_RESULT = "state_result";
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putString(STATE_RESULT, tvResult.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtWidth = findViewById(R.id.edt_width);
+        edtHeight = findViewById(R.id.edt_height);
+        edtLength = findViewById(R.id.edt_lenght);
+        btnCalculate = findViewById(R.id.btn_calculate);
+        tvResult = findViewById(R.id.tv_result);
+
+        btnCalculate.setOnClickListener(this);
+
+        if(savedInstanceState != null){
+            String result = savedInstanceState.getString(STATE_RESULT);
+            tvResult.setText(result);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_calculate){
+            String inputLength = edtLength.getText().toString().trim();
+            String inputWidth = edtWidth.getText().toString().trim();
+            String inputHeight = edtHeight.getText().toString().trim();
+
+            boolean isEmptyFields = false;
+            boolean isInvalidDouble = false;
+
+            if(TextUtils.isEmpty(inputLength)){
+                isEmptyFields = true;
+                edtLength.setError("Field ini tidak boleh kosong");
+            }
+
+            if(TextUtils.isEmpty(inputWidth)){
+                isEmptyFields = true;
+                edtWidth.setError("Field ini tidak boleh kosong");
+            }
+
+            if(TextUtils.isEmpty(inputHeight)){
+                isEmptyFields = true;
+                edtHeight.setError("Field ini tidak bole kosong");
+            }
+
+            Double lenght = toDouble(inputLength);
+            Double width = toDouble(inputWidth);
+            Double height = toDouble(inputHeight);
+
+            if(lenght == null){
+                isInvalidDouble = true;
+                edtLength.setError("Field ini harus berupa nomer yang valid");
+            }
+
+            if(width == null){
+                isInvalidDouble = true;
+                edtWidth.setError("Field ini harus berupa nomer yang valid");
+            }
+
+            if(height == null){
+                isInvalidDouble = true;
+                edtHeight.setError("Field ini harus berupa nomor yang valid");
+            }
+
+            if(!isEmptyFields && !isInvalidDouble){
+                double volume = lenght * width * height;
+
+                tvResult.setText(String.valueOf(volume));
+            }
+        }
+    }
+
+    private Double toDouble(String str){
+        try{
+            return Double.valueOf(str);
+        }catch(NumberFormatException e){
+            return null;
+        }
+    }
+}
